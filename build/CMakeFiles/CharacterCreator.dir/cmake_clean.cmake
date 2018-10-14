f3a8dcc8157528278c06376552eef51b332c95c6
Torquelus <alari.tork@gmail.com>
2018-10-13 21:10:51 -0400

    Initial Commit

---
+++ build/CMakeFiles/CharacterCreator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CharacterCreator.dir/src/main.cpp.o"
+  "CharacterCreator.pdb"
+  "CharacterCreator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CharacterCreator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
